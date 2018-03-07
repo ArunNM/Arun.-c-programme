439b849e6f1375f2cd77e842ec4560bf67430657
Arun.NM <35687883+ArunNM@users.noreply.github.com>
2018-03-07 14:13:16 +0530

    Rename arun83.c to arun84.c

---
+++ arun84.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf(" enter the number ");
+	scanf("%d", &num);
+	if(num%13 == 0)
+	{
+	printf(" the given number is multible of 13 ");
+	}
+	else
+	{
+	printf(" the given number is not multible of 13 ");
+	}
+	return 0;
+	}
