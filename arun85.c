24a1c9d571a13924dd712084e9d2113905d8fa95
Arun.NM <35687883+ArunNM@users.noreply.github.com>
2018-03-07 14:19:25 +0530

    Create arun85.c

---
+++ arun85.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i , n , m , t ;
+    scanf("%d %d", &n , &m);
+    t = n*m;
+    if(n == m)
+    {
+     printf("%d is perfect square",t);
+    }
+     else
+    {
+     printf("%d is not a perfect square",t);
+    }
+}
+
